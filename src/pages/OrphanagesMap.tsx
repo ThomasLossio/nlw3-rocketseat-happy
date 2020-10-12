b718bdce179effe0009e16a0cec02881ee520a6b
Thomas Lossio <thomaslossio@hotmail.com>
2020-10-12 18:55:26 -0300

    Add orphanage map with openstreetmap

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
-function OrphanegesMap() {
+import 'leaflet/dist/leaflet.css'
+
+import mapMarkerImg from '../images/local.svg'
+
+import '../styles/pages/orphanages-map.css'
+
+function OrphanagesMap() {
   return (
-    <h1>Hello world</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Juazeiro do Norte</strong>
+          <span>Ceará</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-7.2276202,-39.3318189]}
+        zoom={15}
+        style={{ width: '100%', height: '100%'}}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
   )
 }
 
-export default OrphanegesMap;
+export default OrphanagesMap;
